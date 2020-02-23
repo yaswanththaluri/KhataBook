8b54143adf215ed19b0c0708ed781109959e2e96
T B Yaswanth Kumar <thaluri.official@gmail.com>
2020-02-23 15:56:42 +0530

    Phone Authentication Completed.

--- app/src/main/java/androidapp/yashthaluri/com/khatabook/MainActivity.java
+++ app/src/main/java/androidapp/yashthaluri/com/khatabook/MainActivity.java
@@ -1,14 +1,22 @@
 package androidapp.yashthaluri.com.khatabook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidapp.yashthaluri.com.khatabook.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
 }
