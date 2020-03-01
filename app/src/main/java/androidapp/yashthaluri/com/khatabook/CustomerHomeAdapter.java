042ef98ddc4ee043f8dc909c3c4d7ff7376bb6bc
NMPavan <manikyapavan123@gmail.com>
2020-03-01 21:30:07 +0530

    Merge branch 'YahBranch' of https://github.com/yaswanththaluri/KhataBook into uibranch
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/androidapp/yashthaluri/com/khatabook/CustomerHomeAdapter.java
@@ -0,0 +1,57 @@
+package androidapp.yashthaluri.com.khatabook;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import androidapp.yashthaluri.com.khatabook.Model.CutsomerDetails;
+
+public class CustomerHomeAdapter extends RecyclerView.Adapter<CustomerHomeAdapter.CustomerViewHolder> {
+    Context context;
+    ArrayList<CutsomerDetails> customerdetailsModel;
+
+    public CustomerHomeAdapter(Context context, ArrayList<CutsomerDetails> customerdetailsModel) {
+        this.context = context;
+        this.customerdetailsModel = customerdetailsModel;
+    }
+
+    @NonNull
+    @Override
+    public CustomerHomeAdapter.CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.layout_customer,parent,false);
+        return new CustomerViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomerHomeAdapter.CustomerViewHolder holder, int position) {
+        holder.PersonImage.setImageResource(customerdetailsModel.get(position).getBitmap());
+        holder.personName.setText(customerdetailsModel.get(position).getName());
+        holder.time.setText(customerdetailsModel.get(position).getTime());
+        holder.Amount.setText(customerdetailsModel.get(position).getAmount());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return customerdetailsModel.size();
+    }
+    public class CustomerViewHolder extends RecyclerView.ViewHolder {
+        ImageView PersonImage;
+        TextView personName,time,Amount;
+        public CustomerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            personName=itemView.findViewById(R.id.person_name);
+            PersonImage = itemView.findViewById(R.id.detail_user_image);
+            time =itemView.findViewById(R.id.time);
+            Amount=itemView.findViewById(R.id.amount);
+        }
+    }
+}
---
+++ app/src/main/java/androidapp/yashthaluri/com/khatabook/Model/CutsomerDetails.java
@@ -0,0 +1,45 @@
+package androidapp.yashthaluri.com.khatabook.Model;
+
+public class CutsomerDetails {
+    int Bitmap,amount;
+    String name,time;
+
+    public CutsomerDetails(int bitmap, String amount, String name, String time) {
+        Bitmap = bitmap;
+        this.amount = amount;
+        this.name = name;
+        this.time = time;
+    }
+
+    public int getBitmap() {
+        return Bitmap;
+    }
+
+    public void setBitmap(int bitmap) {
+        Bitmap = bitmap;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
