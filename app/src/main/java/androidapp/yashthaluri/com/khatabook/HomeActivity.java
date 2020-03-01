7dfee209b4dc5598c1cf5b4dc3ba83e2876032cd
NMPavan <manikyapavan123@gmail.com>
2020-03-01 21:28:35 +0530

    Merge branch 'YahBranch' of https://github.com/yaswanththaluri/KhataBook into uibranch
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/androidapp/yashthaluri/com/khatabook/HomeActivity.java
+++ app/src/main/java/androidapp/yashthaluri/com/khatabook/HomeActivity.java
@@ -2,16 +2,22 @@ package androidapp.yashthaluri.com.khatabook;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
+import androidapp.yashthaluri.com.khatabook.Model.CutsomerDetails;
 import androidapp.yashthaluri.com.khatabook.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
-
+    ArrayList<CutsomerDetails> customerdetailsModel;
+    CustomerHomeAdapter customerHomeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +47,24 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.detailRecyclerview.setHasFixedSize(true);
+        binding.detailRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        customerHomeAdapter= new CustomerHomeAdapter(this, customerdetailsModel);
+        binding.detailRecyclerview.setAdapter(customerHomeAdapter);
+        data();
+
+    }
+
+    private void data() {
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
+        customerdetailsModel.add(new CutsomerDetails(R.drawable.ic_home_black_16dp,"0","Manikyapavan,","30 Minutes ago"));
 
     }
 }
